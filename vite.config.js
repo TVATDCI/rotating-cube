0bf1251c2cab59975dcae15b0e43b9223230a505
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2024-12-07 17:20:00 +0100

    added bg audio, with howler

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@tsParticles/engine', '@tsParticles/react'],
-  base: '/rotating-cube/', // This should match your repository name
+      external: ["@tsParticles/engine", "@tsParticles/react"],
+      base: "/roll-cube/", // This should match your repository name
     },
   },
 });
